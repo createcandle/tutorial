e0efd2580d5bddce9e37714b04dfe0f6ed3c9c1f
Candle - the privacy friendly smart home <39312291+createcandle@users.noreply.github.com>
2022-01-31 18:38:45 +0100

    Add files via upload

---
+++ package.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -e
+
+version=$(grep '"version"' manifest.json | cut -d: -f2 | cut -d\" -f2)
+
+# Setup environment for building inside Dockerized toolchain
+[ $(id -u) = 0 ] && umask 0
+
+# Clean up from previous releases
+rm -rf *.tgz *.sha256sum package SHA256SUMS
+
+if [ -z "${ADDON_ARCH}" ]; then
+  TARFILE_SUFFIX=
+else
+  PYTHON_VERSION="$(python3 --version 2>&1 | cut -d' ' -f2 | cut -d. -f 1-2)"
+  TARFILE_SUFFIX="-${ADDON_ARCH}-v${PYTHON_VERSION}"
+fi
+
+# Prep new package
+mkdir -p package
+
+# Put package together
+cp -r LICENSE manifest.json *.py README.md pkg css images js views package/
+find package -type f -name '*.pyc' -delete
+find package -type f -name '._*' -delete
+find package -type d -empty -delete
+rm -rf package/pkg/pycache
+
+# Generate checksums
+echo "generating checksums"
+cd package
+find . -type f \! -name SHA256SUMS -exec shasum --algorithm 256 {} \; >> SHA256SUMS
+cd -
+
+# Make the tarball
+echo "creating archive"
+TARFILE="candle-theme-${version}.tgz"
+tar czf ${TARFILE} package
+
+shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sum
+cat ${TARFILE}.sha256sum
+
+#rm -rf SHA256SUMS package
